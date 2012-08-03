657e0df24bb014d96a3c4e6716045a8184c28b09
0kso <okso.me>
2012-08-03 20:12:33 +0200

    Started basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='appetizer',
+      version='0.1',
+      description='Flexible configuration manager for Python',
+      author='0kso',
+      author_email='mail@okso.me',
+      url='https://github.com/0kso/Appetizer',
+      packages=['appetizer'],
+     )
